a9cc07a29092956e06d3e50c3a840178abcbf58b
Daren Race <daren.race@gmail.com>
2023-11-13 09:04:30 -0800

    wip

--- components/rc_datatable.py
+++ components/rc_datatable.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from .rc_base import Base
 
-
 class DataTable(Base):
     def __init__(self, df: pd.DataFrame, label=None, **kwargs):
         Base.__init__(self, label=label)
@@ -17,9 +16,11 @@ class DataTable(Base):
             styler.set_caption(label)
             
         styler.hide(axis="index")
-        styler.set_table_attributes('class="fancy_table" width="100%"') 
+        styler.set_table_attributes('class="fancy_table" width="100%"')  
         self.table_html = styler.to_html()
         logging.info(f"DataTable {len(df)} rows")
+        
+        
 
     def to_html(self):
         return self.table_html
