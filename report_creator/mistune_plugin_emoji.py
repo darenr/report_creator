1482d673ce3f7ee3ebf239931c7cdaf88d1d4496
Daren Race <daren.race@gmail.com>
2024-11-27 14:07:32 -0800

    begin mistune plugin

---
+++ report_creator/mistune_plugin_emoji.py
@@ -0,0 +1,7 @@
+# https://mistune.lepture.com/en/v2/advanced.html#create-plugins
+# example: https://github.com/lepture/mistune/blob/master/src/mistune/plugins/url.py
+# docs https://carpedm20.github.io/emoji/docs/
+# https://pypi.org/project/emoji/
+# print(emoji.emojize('Python is :thumbs_up:'))
+# list https://carpedm20.github.io/emoji/
+# is_emoji()
