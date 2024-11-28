00c706e91814cee9700ffed81ea128313c0412c7
Daren Race <daren.race@gmail.com>
2024-11-28 10:48:35 -0800

    fix the mistune plugin

--- aa.py
+++ aa.py
@@ -9,21 +9,29 @@ INLINE_EMOJI_PATTERN = r":[A-Za-z0-9._’()_-]+:"
 
 
 def parse_inline_emoji_icon(inline, m, state) -> int:
-    state.append_token({"type": "text", "raw": m.group(0)})
-    return m.end()
+    text = m.group(0)
+    pos = m.end()
+
+    state.append_token(
+        {
+            "type": "emoji_icon_ref",
+            "attrs": {"emoji_str": text},
+        }
+    )
+    return pos
 
 
-def render_inline_emoji_icon(renderer, text: str) -> str:
-    return f"{emoji.emojize(text)}"
+def render_inline_emoji_icon(renderer, emoji_str: str) -> str:
+    return f"{emoji.emojize(emoji_str)}"
 
 
-def emoji(md) -> None:
-    md.inline.register("emoji", INLINE_EMOJI_PATTERN, parse_inline_emoji_icon)
+def emoji_plugin(md) -> None:
+    md.inline.register("emoji_plugin", INLINE_EMOJI_PATTERN, parse_inline_emoji_icon)
     if md.renderer and md.renderer.NAME == "html":
         md.renderer.register("emoji_icon_ref", render_inline_emoji_icon)
 
 
 import mistune
 
-markdown = mistune.create_markdown(plugins=[emoji])
-print(markdown(":smile:"))  # <p>😄</p>
+markdown = mistune.create_markdown(plugins=[emoji_plugin])
+print(markdown("We :love: :pizza:"))  # <p>😄</p>
