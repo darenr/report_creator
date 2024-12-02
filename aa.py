35ad01245d472635da9db0a36af28e2854173c36
Daren Race <daren.race@gmail.com>
2024-12-02 14:38:44 -0800

    more with emoji plugin

--- aa.py
+++ aa.py
@@ -1,37 +1,42 @@
 import emoji
 
-# https://github.com/lepture/mistune/blob/master/src/mistune/plugins/url.py
-
-
-__all__ = ["emoji_icon"]
-
 INLINE_EMOJI_PATTERN = r":[A-Za-z0-9._’()_-]+:"
 
 
 def parse_inline_emoji_icon(inline, m, state) -> int:
-    text = m.group(0)
-    pos = m.end()
-
     state.append_token(
         {
             "type": "emoji_icon_ref",
-            "attrs": {"emoji_str": text},
+            "attrs": {"emoji_str": m.group(0)},
         }
     )
-    return pos
+    return m.end()
 
 
 def render_inline_emoji_icon(renderer, emoji_str: str) -> str:
-    return f"{emoji.emojize(emoji_str)}"
+    return f"{emoji.emojize(emoji_str, variant="emoji_type", language="en")}"
 
 
-def emoji_plugin(md) -> None:
-    md.inline.register("emoji_plugin", INLINE_EMOJI_PATTERN, parse_inline_emoji_icon)
+def emojis(md) -> None:
+    md.inline.register("emojis", INLINE_EMOJI_PATTERN, parse_inline_emoji_icon)
     if md.renderer and md.renderer.NAME == "html":
         md.renderer.register("emoji_icon_ref", render_inline_emoji_icon)
 
 
 import mistune
 
-markdown = mistune.create_markdown(plugins=[emoji_plugin])
-print(markdown("We :love: :pizza:"))  # <p>😄</p>
+markdown = mistune.create_markdown(plugins=[emojis])
+
+with open("emoji.html", "w", encoding="utf-8") as f:
+    print(
+        """
+        <!DOCTYPE html>
+            <html>
+            <body>
+            <p>
+    """,
+        file=f,
+    )
+    print(markdown("We :red_heart: all :pizza: in Engineering"), file=f)
+
+    print("</p></body></html>", file=f)
