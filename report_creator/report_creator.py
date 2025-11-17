379560c4dd014d9f28ac75abb8dbd4743b149218
Daren Race <daren.race@gmail.com>
2025-11-17 13:28:11 -0800

    force error to test py version tests tests

--- report_creator/report_creator.py
+++ report_creator/report_creator.py
@@ -2078,11 +2078,13 @@ class Sql(Language):
         for statement_pattern in BLOCK_STATEMENTS:
             # Using a lambda that captures the current state of 'original_sql_for_check'
             def current_replacer(m, osql=original_sql_for_check):
-                return (
-                ""
-                if osql.lower().lstrip().startswith(m.group(1).lower())
-                else "\n" + m.group(1).upper() + "\n\t"
-            )
+                return f"{'' if osql.lower().lstrip().startswith(m.group(1).lower()) else '\n'}{m.group(1).upper()}\n\t"
+
+                # return (
+                #     ""
+                #     if osql.lower().lstrip().startswith(m.group(1).lower())
+                #     else "\n" + m.group(1).upper() + "\n\t"
+                # )
 
             # Refined pattern to better handle keywords at the start or surrounded by whitespace
             sql = re.sub(
